38a8591117dc2e6eefea4bbdbadf3eaac10cc439
sayyidalharis <101546647+sayyidalharis@users.noreply.github.com>
2022-05-26 07:09:50 +0700

    ic

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
 import React from 'react'
-import {render} from 'react-dom'
-import {createUseStyles} from 'react-jss'
+import logo from './logo.svg';
+import './App.css';
 
-// Create your Styles. Remember, since React-JSS uses the default preset,
-// most plugins are available without further configuration needed.
-const useStyles = createUseStyles({
-  myButton: {
-    color: 'green',
-    margin: {
-      // jss-plugin-expand gives more readable syntax
-      top: 5, // jss-plugin-default-unit makes this 5px
-      right: 0,
-      bottom: 0,
-      left: '1rem'
-    },
-    '& span': {
-      // jss-plugin-nested applies this to a child span
-      fontWeight: 'bold' // jss-plugin-camel-case turns this into 'font-weight'
-    }
-  },
-  myLabel: {
-    fontStyle: 'italic'
-  }
-})
-
-// Define the component using these styles and pass it the 'classes' prop.
-// Use this to assign scoped class names.
-const Button = ({children}) => {
-  const classes = useStyles()
+function App() {
   return (
-    <button className={classes.myButton}>
-      <span className={classes.myLabel}>{children}</span>
-    </button>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and saveszssz to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-const App = () => <Button>Submit</Button>
-
-render(<App />, document.getElementById('root'))
 export default App;
